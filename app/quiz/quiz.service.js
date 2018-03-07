d518dd85fc8b6777336e6aa59a81a5c4b388a094
Sam Bleckley <sam@climatum.com>
2018-03-06 20:00:20 -0500

    Add service

---
+++ app/quiz/quiz.service.js
@@ -0,0 +1,12 @@
+angular.module("finalAssessment").service("QuizService", function() {
+  this.questions = [];
+  this.results = [];
+  
+  this.setAnswer = function(questionNumber, answerNumber) {
+
+  }
+
+  this.getAnswer = function(questionNumber) {
+    
+  }
+});
--- app/sample-component/sample.component.js
+++ app/sample-component/sample.component.js
@@ -1,6 +1,6 @@
 angular.module("finalAssessment").component("mySample", {
   template: require('./sample.html'),
   controller: function() {
-    this.text= "component";
+    this.text = "component";
   }
 });
