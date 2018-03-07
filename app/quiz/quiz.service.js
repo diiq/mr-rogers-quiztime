3a0f11e29c5391d27c81a720a0808808fece248d
Sam Bleckley <sam@climatum.com>
2018-03-06 20:23:24 -0500

    Add a testing component

--- app/quiz/quiz.service.js
+++ app/quiz/quiz.service.js
@@ -1,5 +1,46 @@
+class Question {
+  constructor(text, answers) {
+    this.text = text;
+    this.answers = answers;
+    this.userAnswer = null;
+  }
+
+  setAnswer(answerNumber) {
+    this.userAnswer = answerNumber;
+  }
+
+  clearAnswer() {
+    this.userAnswer = null;
+  }
+}
+
+class Answer {
+  constructor(type, item, effect) {
+    this.type = type;
+    this.effect = effect;
+    if (type === "text") {
+      this.text = item;
+    } else if (type === "image") {
+      this.imageUrl = item;
+    }
+  }
+}
+
 angular.module("finalAssessment").service("QuizService", function() {
-  this.questions = [];
+  this.questions = [
+    new Question("Where would you go for vacation?", [
+      new Answer("image", "https://reference-board.s3.amazonaws.com/references/images/8f2/086/5d-/medium/data?1512329298", {}),
+      new Answer("image", "https://reference-board.s3.amazonaws.com/references/images/9c6/7ae/96-/medium/data?1508674664", {}),
+      new Answer("image", "https://reference-board.s3.amazonaws.com/references/images/037/599/2c-/medium/data?1518013403", {}),
+      new Answer("image", "https://reference-board.s3.amazonaws.com/references/images/432/823/15-/medium/open-uri20171126-4-1ga4ew?1511716550", {})
+    ]),
+    new Question("What do you like to eat?", [
+      new Answer("text", "A sandwich", {}),
+      new Answer("text", "A pie", {}),
+      new Answer("text", "A scotch egg", {}),
+      new Answer("text", "A loaf of bread, a jug of wine", {})
+    ])
+  ];
   this.results = [];
   
   this.setAnswer = function(questionNumber, answerNumber) {
@@ -7,6 +48,6 @@ angular.module("finalAssessment").service("QuizService", function() {
   }
 
   this.getAnswer = function(questionNumber) {
-    
+
   }
 });
--- app/sample-component/sample.component.js
+++ app/sample-component/sample.component.js
@@ -1,6 +1,9 @@
 angular.module("finalAssessment").component("mySample", {
   template: require('./sample.html'),
-  controller: function() {
-    this.text = "component";
-  }
+  controller: ["QuizService", function(QuizService) {
+    this.question1 = QuizService.questions[0];
+    this.question2 = QuizService.questions[1];
+    this.answer1 = this.question1.answers[0];
+    this.answer2 = this.question2.answers[0];
+  }]
 });
